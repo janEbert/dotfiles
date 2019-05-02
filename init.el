cd3d0ea18412039af9983375cd423aab1a6d5852
janEbert <janpublicebert@posteo.net>
2019-05-03 00:15:11 +0200

    Update .emacs and rename it to init.el

---
+++ init.el
@@ -0,0 +1,45 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(backup-by-copying t)
+ '(backup-directory-alist (quote (("" . "~/.emacs.d/backups/"))))
+ '(frame-background-mode (quote dark))
+ '(kept-new-versions 6)
+ '(kept-old-versions 4)
+ '(package-selected-packages (quote (color-theme-solarized julia-mode)))
+ '(vc-make-backup-files t)
+ '(version-control t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq visible-bell 1)
+
+(add-to-list 'default-frame-alist
+             '(font . "DejaVu Sans Mono-11"))
+
+(let ((basedir "~/.emacs.d/themes/"))
+  (dolist (f (directory-files basedir))
+    (if (and (not (or (equal f ".") (equal f "..")))
+             (file-directory-p (concat basedir f)))
+        (add-to-list 'custom-theme-load-path (concat basedir f)))))
+
+(load-theme 'solarized t)
+

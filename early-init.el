ee842d6f598e2e2d263cc43576d3d60d906f555d
janEbert <janpublicebert@posteo.net>
2020-05-17 02:48:07 +0200

    Add early-init.el

---
+++ early-init.el
@@ -0,0 +1,12 @@
+;; -*- mode: emacs-lisp; lexical-binding: t -*-
+
+(setq gc-cons-threshold 402653184
+	  gc-cons-percentage 0.6)
+
+(setq package-quickstart t)
+;; (setq package-enable-at-startup nil)
+;; (setq package-load-list '(
+;; 						  (package-to-disable nil)
+;; 						  all))
+
+;;; early-init.el ends here

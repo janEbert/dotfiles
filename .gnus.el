81ea4e848b9bb638c608b997cd23551ba78f0f9f
janEbert <janpublicebert@posteo.net>
2022-03-04 17:23:11 +0100

    Add commented Gnus config
    Pretty WIP

--- .gnus.el
+++ .gnus.el
@@ -1,1 +1,15 @@
+(setq gnus-select-method '(nnnil))
+;; (setq gnus-secondary-select-methods
+;; 	  '(
+;; 		(nnimap "<address>"
+;; 				(nnimap-user "user")
+;; 				(nnimap-server-port 993)
+;; 				(nnimap-stream ssl)
+;; 				(nnmail-expiry-target "nnimap+<address>:Trash")
+;; 				)
+;; 		))
 
+;; (setq gnus-icalendar-org-capture-file nil)
+;; (setq gnus-icalendar-org-capture-headline '("Calendar"))
+;; (require 'gnus-icalendar)
+;; (gnus-icalendar-org-setup)

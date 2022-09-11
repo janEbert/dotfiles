27a18e8d1d370bc53520c2551f24a835ee481b02
janEbert <janpublicebert@posteo.net>
2022-09-11 15:44:37 +0200

    Do not invert SVG images

--- dark-mode.js
+++ dark-mode.js
@@ -1,5 +1,5 @@
 // Dark (inversion)
-javascript:(function(d){d.head.appendChild(d.createElement('style')).innerText='html,img,video{-webkit-filter:invert(1)hue-rotate(180deg);filter:invert(1)hue-rotate(180deg)}body{background:#000}'})(document);
+javascript:(function(d){d.head.appendChild(d.createElement('style')).innerText='html,img:not([src$=".svg"]),video{-webkit-filter:invert(1)hue-rotate(180deg);filter:invert(1)hue-rotate(180deg)}body{background:#000}'})(document);
 
 // Light (revert inversion)
 javascript:(function(h){h.removeChild(h.lastChild)})(document.head);
